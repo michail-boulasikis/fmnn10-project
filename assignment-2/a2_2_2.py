bbda1e2cc677a419751fd8e4f52ee77ac6058406
Michail Boulasikis <michail.boulasikis@cs.lth.se>
2023-11-28 12:55:38 +0100

    beautified graphs and added titles to them

--- assignment-2/a2_2_2.py
+++ assignment-2/a2_2_2.py
@@ -21,15 +21,23 @@ def potential_4(x):
 
 if __name__ == '__main__':
     potentials = [potential_1, potential_2, potential_3, potential_4]
+    titles = [
+        "$V(x) = 700(0.5 - \\|x - 0.5\\|)}$",
+        "$V(x) = 800\\sin^2(\\pi x)$",
+        "$V(x) = 700x(1-x)$",
+        "$V(x) = 700(1 - \\cos(8\\pi x))$"
+    ]
 
     fig, ax = plt.subplots(1, len(potentials))
     for i, pot in enumerate(potentials):
         prob = sturm_liouville.SturmLiouvilleProblem(pot, neumann_final=False)
         x, v, vec = prob.modes()
         ax[i].plot(x, -pot(x), label="Potential")
+        ax[i].set_title(titles[i])
         ax_twin = ax[i].twinx()
         for j in range(6):
             ax_twin.plot(x, 50 * np.power(np.abs(vec[:, j]), 2) - 0.0075 * np.real(v[j]),
                          label=f"Mode {i}: Energy level: {np.real(v[j]):.2}")
+            ax_twin.set_yticklabels([])
         ax[i].legend()
     plt.show()
