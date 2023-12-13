334e6442ebd4ac3c8e1abdbae673f3d5fa02bacc
MelanieInky <melanie.fournier234@gmail.com>
2023-12-13 19:03:08 +0100

    Changed so that it integrates exactly in [0,1] . Better presentation.

--- assignment-3/task_1_1.py
+++ assignment-3/task_1_1.py
@@ -5,19 +5,33 @@ import matrices
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    N = 19
-    m = 0.52
+    t0 = 0
+    t1 = 1
+    N = 39
+    m = 0.49 #Desired
     x = np.linspace(0, 1, N+2)
     dx = x[1] - x[0]
-    print(f"dx = {dx}")
+    dt_desired = m * dx**2 #desired
+    M = int(1+ (t1-t0)/dt_desired)
+    dt = (t1-t0)/(M-1) #Actual value to get an equidistant grid
+    mu = dt*dx**-2
     y0 = x * (x - 1)
     y0 = y0[1:-1]
-    prob = integrator.EulerIntegrator(matrices.T(N, dx), y0)
-    dt = m * dx**2
-    prob.apply_discretization_scheme(dt)
+    
+    print("------ Diffusion equation ------")
+    print(f"Integrated in the time interval [{t0},{t1}]")
+    print("Parameters:")
+    print(f"N = {N} ; M = {M}")
+    print(f"dt = {dt:.4g} ; dx = {dx:.4g}; mu = {mu:.8g}")
+    print('--------------------------------')
+    prob = integrator.EulerIntegrator(matrices.T(N, dx), y0,t0,t1)
+    #prob.apply_discretization_scheme(dt)
+    prob.apply_discretization_scheme(M)
     t, y, _ = prob.solve()
     y_padded = np.concatenate((np.zeros((*t.shape, 1)), y, np.zeros((*t.shape, 1))), axis=1)
     fig, ax = plt.subplots(subplot_kw={"projection" : "3d"})
     T, X = np.meshgrid(x, t)
     surf = ax.plot_surface(T, X, y_padded)
+    plt.xlabel("x")
+    plt.ylabel('t')
     plt.show()
